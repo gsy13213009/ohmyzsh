aefbab4b213e461d0953376ccac3adf283a5c5d1
gsy <gsy@gsy.com>
2022-11-25 23:39:05 +0800

    change git alias and ys theme

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -70,9 +70,9 @@ alias gapt='git apply --3way'
 
 alias gb='git branch'
 alias gba='git branch -a'
-alias gbd='git branch -d'
 alias gbda='git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch -d 2>/dev/null'
 alias gbD='git branch -D'
+alias gbd='git branch -D'
 alias gbl='git blame -b -w'
 alias gbnm='git branch --no-merged'
 alias gbr='git branch --remote'
@@ -106,8 +106,8 @@ compdef _git gccd=git-clone
 alias gcl='git clone --recurse-submodules'
 alias gclean='git clean -id'
 alias gpristine='git reset --hard && git clean -dffx'
-alias gcm='git checkout $(git_main_branch)'
 alias gcd='git checkout $(git_develop_branch)'
+alias gcm='git commit -m'
 alias gcmsg='git commit -m'
 alias gco='git checkout'
 alias gcor='git checkout --recurse-submodules'
@@ -120,6 +120,7 @@ alias gcss='git commit -S -s'
 alias gcssm='git commit -S -s -m'
 
 alias gd='git diff'
+alias gdd='git diff --cached'
 alias gdca='git diff --cached'
 alias gdcw='git diff --cached --word-diff'
 alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
@@ -169,15 +170,64 @@ function ggl() {
 }
 compdef _git ggl=git-checkout
 
-function ggp() {
-  if [[ "$#" != 0 ]] && [[ "$#" != 1 ]]; then
-    git push origin "${*}"
-  else
-    [[ "$#" == 0 ]] && local b="$(git_current_branch)"
-    git push origin "${b:=$1}"
-  fi
+ggp() {
+    local name=$1
+    local force=$2
+    [[ "$name" == "-f" ]] && name=$2 && force=$1
+    [[ "$#" == 1 || "$#" == 2 ]] && local target="`git_current_branch`:`echo $name | sed s#^origin/##g`"
+    echo "git push origin ${target:=`git_current_branch`} $force"
+    git push origin "${target:=`git_current_branch`}" $force
+    echo https://git.zhixiaoyoufang.com/ts/ts_mini_flutter/compare/master...ts:$name
 }
+ggcp() {
+  local branch=$1
+  local commit=$2
+  [[ ! "$branch" =~ "/" ]] && branch=$2 && commit=$1
+  local stat=`git status -s`
+  if [[ -n "$stat" ]];then
+    echo 'You have some changes'
+    return
+  fi
+  gco -b "commit_temp_branch"
+  git reset --hard origin/master
+  gcp $commit
+  ggp $branch '-f'
+  gco -
+  gbd "commit_temp_branch"
+}
+ggr() {
+  local name=$1
+  local push=$2
+  if [[ "$2" == "-p" ]] || [[ "$2" == "-f" ]];then
+    [[ "$2" == "-p" ]] && ggp $1
+    [[ "$2" == "-f" ]] && ggp $1 -f
+    return
+  fi
+  [[ -z "$1" ]] && git reset --hard origin/`git_current_branch` && return
+  local target=`echo "$name" | sed s#^origin/##g`
+  target="origin/$target"
+  git reset --hard $target
+}
+ggb() {
+  git reset --hard $1
+}
+alias ggd='(){[[ "$#" == 1 ]] && git push origin :`echo $1 | sed s#^origin/##g`}'
+#alias ggr='(){git reset --hard ${1:=origin/`git_current_branch`}}'
+
 compdef _git ggp=git-checkout
+compdef _git ggd=git-checkout
+compdef _git ggr=git-checkout
+compdef _git ggb=git-checkout
+
+# Problem: ggp zsh:12: command not found: __git-checkout_main
+#
+# The problem here is that the official Git completion was not prepared for 'complete_aliases
+# This way you can manually define the completion with:
+# compdef _git gco=git_checkout
+# However, if you want to use zsh's format: gco=git-checkout and for it to work in the currently distributed version of Git, you can do this:
+# alias __git-checkout_main=_git_checkout
+# https://github.com/ohmyzsh/ohmyzsh/issues/2394
+alias __git-checkout_main=_git_checkout
 
 function ggpnp() {
   if [[ "$#" == 0 ]]; then
@@ -203,9 +253,22 @@ alias gpsup='git push --set-upstream origin $(git_current_branch)'
 
 alias ghh='git help'
 
-alias gignore='git update-index --assume-unchanged'
-alias gignored='git ls-files -v | grep "^[[:lower:]]"'
 alias git-svn-dcommit-push='git svn dcommit && git push github $(git_main_branch):svntrunk'
+alias gignore='git update-index --skip-worktree'
+alias gignored='git ls-files -t | grep "^S"'
+alias gunignore='git update-index --no-skip-worktree'
+_gignoreall() {
+  local no_staged=`git diff --name-only --relative`
+  echo "ignored: $no_staged"
+  gignore `echo $no_staged`
+}
+_gunignoreall() {
+  local ignored=`gignored | sed 's/^S //g' | xargs`
+  echo "unignore: $ignored"
+  gunignore `echo $ignored`
+}
+alias gallignore='_gignoreall'
+alias gallunignore='_gunignoreall'
 
 alias gk='\gitk --all --branches &!'
 alias gke='\gitk --all $(git log -g --pretty=%h) &!'
@@ -221,9 +284,9 @@ alias glol="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgre
 alias glols="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat"
 alias glod="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
 alias glods="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
-alias glola="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
-alias glog='git log --oneline --decorate --graph'
-alias gloga='git log --oneline --decorate --graph --all'
+alias gloga="git log --graph --pretty=format:'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(yellow)%d%Creset %s %Cgreen(%ci)'"
+alias glogn="git log --pretty=format:'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(yellow)%d%Creset %s %Cgreen(%ci)'"
+alias glog="git log --graph --pretty=format:'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(yellow)%d%Creset %s %Cgreen(%ci)' --no-merges"
 alias glp="_git_log_prettily"
 
 alias gm='git merge'
@@ -237,12 +300,13 @@ alias gp='git push'
 alias gpd='git push --dry-run'
 alias gpf='git push --force-with-lease'
 alias gpf!='git push --force'
+alias gpt='git push origin --tags'
 alias gpoat='git push origin --all && git push origin --tags'
 alias gpr='git pull --rebase'
 alias gpu='git push upstream'
 alias gpv='git push -v'
 
-alias gr='git remote'
+alias gr='git reset'
 alias gra='git remote add'
 alias grb='git rebase'
 alias grba='git rebase --abort'
@@ -254,11 +318,12 @@ alias grbom='git rebase origin/$(git_main_branch)'
 alias grbo='git rebase --onto'
 alias grbs='git rebase --skip'
 alias grev='git revert'
-alias grh='git reset'
 alias grhh='git reset --hard'
 alias groh='git reset origin/$(git_current_branch) --hard'
 alias grm='git rm'
 alias grmc='git rm --cached'
+alias grh='git reset --hard'
+alias grH='git reset HEAD^'
 alias grmv='git remote rename'
 alias grrm='git remote remove'
 alias grs='git restore'
@@ -284,6 +349,8 @@ is-at-least 2.13 "$git_version" \
   && alias gsta='git stash push' \
   || alias gsta='git stash save'
 
+alias gs='git status ${1:=.}'
+alias gsta='git stash save'
 alias gstaa='git stash apply'
 alias gstc='git stash clear'
 alias gstd='git stash drop'
@@ -302,8 +369,8 @@ alias gts='git tag -s'
 alias gtv='git tag | sort -V'
 alias gtl='gtl(){ git tag --sort=-v:refname -n -l "${1}*" }; noglob gtl'
 
-alias gunignore='git update-index --no-assume-unchanged'
 alias gunwip='git log -n 1 | grep -q -c "\--wip--" && git reset HEAD~1'
+alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
 alias gup='git pull --rebase'
 alias gupv='git pull --rebase -v'
 alias gupa='git pull --rebase --autostash'
@@ -343,3 +410,12 @@ function grename() {
 }
 
 unset git_version
+# Problem: ggp zsh:12: command not found: __git-checkout_main
+#
+# The problem here is that the official Git completion was not prepared for 'complete_aliases
+# This way you can manually define the completion with:
+# compdef _git gco=git_checkout
+# However, if you want to use zsh's format: gco=git-checkout and for it to work in the currently distributed version of Git, you can do this:
+# alias __git-checkout_main=_git_checkout
+# https://github.com/ohmyzsh/ohmyzsh/issues/2394
+alias __git-checkout_main=_git_checkout
--- themes/ys.zsh-theme
+++ themes/ys.zsh-theme
@@ -7,7 +7,7 @@
 
 # VCS
 YS_VCS_PROMPT_PREFIX1=" %{$reset_color%}on%{$fg[blue]%} "
-YS_VCS_PROMPT_PREFIX2=":%{$fg[cyan]%}"
+YS_VCS_PROMPT_PREFIX2=": %{$fg[cyan]%}"
 YS_VCS_PROMPT_SUFFIX="%{$reset_color%}"
 YS_VCS_PROMPT_DIRTY=" %{$fg[red]%}x"
 YS_VCS_PROMPT_CLEAN=" %{$fg[green]%}o"
